e08c20caf8462c170805fe4ed98046ba7053ef89
Ivan <Shibkikh.I.V@sberbank.ru>
2026-03-17 23:16:33 +0300

    fix time

--- finam/convert_quote.go
+++ finam/convert_quote.go
@@ -1,13 +1,15 @@
 package finam
 
 import (
+	"time"
+
 	"github.com/FinamWeb/finam-trade-api/go/grpc/tradeapi/v1/marketdata"
 	"github.com/nskforward/trading/types"
 )
 
 func convertQuote(in *marketdata.Quote) types.Quote {
 	return types.Quote{
-		Time:   in.Timestamp.AsTime(),
+		Time:   time.Now(),
 		Symbol: in.Symbol,
 		Ask:    convertDecimal(in.Ask),
 		Bid:    convertDecimal(in.Bid),
